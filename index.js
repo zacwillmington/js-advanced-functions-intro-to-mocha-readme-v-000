ff898cfb28a0c38336f4d01b420dddd8f9622f33
Zac Willmington <zac.willmington@gmail.com>
2018-09-25 00:46:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+    return true;
+}
+
+function sayHey(){
+    return "Hey!";
+}
+
+function sayHeyFriend(name){
+    return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(icecream) {
+    return `I love ${icecream}`;
+}
+
+function shouting(str){
+    return  str.toUpperCase();
+}
+
+function roundDown(num){
+    return Math.floor(num);
+}
